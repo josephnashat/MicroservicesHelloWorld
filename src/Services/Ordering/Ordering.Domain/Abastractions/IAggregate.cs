dec5d5070aeb8b2734013f66af41e8f9363059ce
Joseph Nashat <joseph_nashat@hotmail.com>
2025-02-20 14:57:56 +0200

    End of Section 14

---
+++ src/Services/Ordering/Ordering.Domain/Abastractions/IAggregate.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Abastractions;
+public interface IAggregate<T> : IAggregate, IEntity<T>
+{
+
+}
+public interface IAggregate: IEntity
+{
+    public IReadOnlyList<IDomainEvent> DomainEvents { get; }
+
+}
---
+++ src/Services/Ordering/Ordering.Domain/Abastractions/IDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿
+using MediatR;
+
+namespace Ordering.Domain.Abastractions;
+
+public interface IDomainEvent: INotification
+{
+    public Guid EventId => Guid.NewGuid();
+    public DateTime OccuredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName!;
+
+}
